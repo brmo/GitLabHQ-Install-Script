ec12ccf245aa1c205e25d3ee608270a89a80cb51
Brian Morris <brian.morris.personal@gmail.com>
2012-05-16 11:57:12 -0700

    Updated installScript.sh

--- installScript.sh
+++ installScript.sh
@@ -5,9 +5,9 @@ apt-get upgrade
 
 apt-get install -y git-core wget curl gcc checkinstall libxml2-dev libxslt-dev sqlite3 libsqlite3-dev libcurl4-openssl-dev libreadline5-dev libc6-dev libssl-dev libmysql++-dev make build-essential zlib1g-dev libicu-dev redis-server openssh-server git-core python-dev python-pip
 
-wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p290.tar.gz
-tar xfvz ruby-1.9.2-p290.tar.gz
-cd ruby-1.9.2-p290
+wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p194.tar.gz
+tar xfvz ruby-1.9.3-p194.tar.gz
+cd ruby-1.9.3-p194
 ./configure
 make
 sudo make install
